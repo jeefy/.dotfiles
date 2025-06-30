c23aae94d958075423b49d29e062e60e7d5528b5
Jeffrey Sica <me@jeefy.dev>
2025-06-30 11:55:54 -0500

    auto-update

--- dotUpdate.sh
+++ dotUpdate.sh
@@ -1,33 +1,41 @@
 #!/usr/bin/env bash
 
-if [ ! -e /tmp/dotfiles ]; then
-    echo "Fresh boot, doing some house cleaning";
-    cd ~/.dotfiles || exit
-    git fetch
-    if [[ $(git diff main origin/main | wc -l) -ne 0 ]]; then
-        echo "Updating dotfiles";
-        git pull origin main
-        echo "Done updating dotfiles";
-        echo "Installing dotfiles/apps";
-        brew bundle --file=~/.dotfiles/Brewfile
-        flatpak install -y -u "$(cat ~/.dotfiles/Flatfile)"
-        cat ~/.dotfiles/CodeExtensionsfile | xargs -L 1 echo code --install-extension
-        echo "Done installing dotfiles/apps";
+if [ -e /tmp/dotfiles.lock ]; then
+    echo "Dotfiles update script is already running, exiting.";
+else
+    touch /tmp/dotfiles.lock
+
+    if [ ! -e /tmp/dotfiles ]; then
+        echo "Fresh boot, doing some house cleaning";
+        cd ~/.dotfiles || exit
+        git fetch
+        if [[ $(git diff main origin/main | wc -l) -ne 0 ]]; then
+            echo "Updating dotfiles";
+            git pull origin main
+            echo "Done updating dotfiles";
+            echo "Installing dotfiles/apps";
+            brew bundle --file=~/.dotfiles/Brewfile
+            flatpak install -y -u "$(cat ~/.dotfiles/Flatfile)"
+            cat ~/.dotfiles/CodeExtensionsfile | xargs -L 1 echo code --install-extension
+            echo "Done installing dotfiles/apps";
+        fi
+
+        topgrade --disable=shell --yes
+
+        brew bundle dump --force --file=~/.dotfiles/Brewfile
+        flatpak list --app -u --columns=application > ~/.dotfiles/Flatfile
+        code --list-extensions > ~/.dotfiles/CodeExtensionsfile
+        dconf dump /org/gnome/Ptyxis/ > ~/.dotfiles/dconf/ptyxis.conf
+        gnome-extensions list --user --active --enabled > ~/.dotfiles/gnome-extensions-list
+        git add .
+        git commit -m "auto-update"
+        git push origin main
+
+        echo "Complete. Bluefin is ready to go!";
+
+        cd ~ || exit
+        touch /tmp/dotfiles
     fi
 
-    topgrade --disable=shell --yes
-
-    brew bundle dump --force --file=~/.dotfiles/Brewfile
-    flatpak list --app -u --columns=application > ~/.dotfiles/Flatfile
-    code --list-extensions > ~/.dotfiles/CodeExtensionsfile
-    dconf dump /org/gnome/Ptyxis/ > ~/.dotfiles/dconf/ptyxis.conf
-    gnome-extensions list --user --active --enabled > ~/.dotfiles/gnome-extensions-list
-    git add .
-    git commit -m "auto-update"
-    git push origin main
-
-    echo "Complete. Bluefin is ready to go!";
-
-    cd ~ || exit
-    touch /tmp/dotfiles
+    rm /tmp/dotfiles.lock
 fi
